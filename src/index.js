3383be29596220144297aa7ef7de43bffce31a3f
Carlos Wagner <wagner30023@gmail.com>
2021-05-12 09:36:48 -0300

    carrendo html viaa ajax

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Arquivos SASS
+
+import './scss/index.scss'
+
+// Dependencias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
